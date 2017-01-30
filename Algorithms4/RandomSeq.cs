b345bc759f0a861d4cbeeaea07e7c7091fd04854
Brian Martinez <brickbat@hotmail.com>
2017-01-29 22:47:58 -0700

    Added RandomSeq class Added test client to use with library

---
+++ Algorithms4/RandomSeq.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms4
+{
+    public class RandomSeq
+    {
+        private static Random rnd;
+
+        // do not instantiate
+        private RandomSeq()
+        {
+
+        }
+
+        /// <summary>
+        /// Returns n uniformly random real numbers in [lo, hi)
+        /// </summary>
+        /// <param name="n">number of double values to return</param>
+        /// <param name="lo">double lower bound</param>
+        /// <param name="hi">double higher bound</param>
+        /// <returns>array of random double values</returns>
+        public static double[] GenerateRandomSequence(int n, double lo, double hi)
+        {
+            if (!(lo < hi))
+            {
+                throw new Exception("lo value must be lower than hi value");
+            }
+
+            double[] rndSeq = new double[n];
+
+            // create pseudo-random number generator
+            rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                double x = lo + rnd.NextDouble() * (hi - lo);
+                rndSeq[i] = x;
+            }
+
+            return rndSeq;
+        }
+    }
+}
---
+++ algs4TestClient/TestClient.cs
@@ -0,0 +1,34 @@
+﻿using Algorithms4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algs4TestClient
+{
+    class TestClient
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double[] rndNums = RandomSeq.GenerateRandomSequence(5, 10.0, 11.0);
+
+                for (int i = 0; i < rndNums.Length; i++)
+                {
+                    Console.WriteLine(rndNums[i]);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR: " + e.Message);
+            }
+            finally
+            {
+                Console.Write("Press any key to continue...");
+                var foo = Console.ReadKey();
+            }
+        }
+    }
+}
